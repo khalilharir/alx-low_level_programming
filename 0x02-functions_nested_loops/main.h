543327b147c1f53e73156716973b10386d0852e4
khalilharir <khalil.harir2@gmail.com>
2023-06-21 01:37:42 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,6 +12,8 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);
-
+void print_to_98(int n);
+void print_times_table(int n);
+int _natural(void);
+void fibonacci(void);
 #endif
