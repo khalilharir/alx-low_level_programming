4bc766ff0cab8811d30e4913b3a54bf1c005f4fe
khalilharir <khalil.harir2@gmail.com>
2023-07-12 14:30:58 -0700

    free grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -6,7 +6,7 @@
  * @grid: 2d grid
  * @height: height dimension of grid
  * Description: frees memory of grid
- * Return: NULL
+ * Return: nothing
  *
  */
 void free_grid(int **grille, int height)
@@ -19,4 +19,3 @@ void free_grid(int **grille, int height)
 	}
 	free(grille);
 }
-
