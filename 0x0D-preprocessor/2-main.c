70a0b11231f050f2817e1e15e3a220a535a1110b
khalilharir <khalil.harir2@gmail.com>
2023-07-17 12:37:19 -0700

    main c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file it 
- * was compiled from, 
- * followed by a new line .
- * Returns: Always 0 (Success)
+ * main - it prints the name of the file it was compiled from 
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
