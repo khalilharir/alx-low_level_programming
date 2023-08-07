7adb4572298c4e45068d2a781b859c5425741de1
khalilharir <khalil.harir2@gmail.com>
2023-08-07 08:23:55 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - copy content from files
+* main - copy info from file_from to file_to.
 * @ac: number of arguments
 * @av: array of arguments
 * Return: Always 0.
@@ -15,18 +15,15 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97); 
-        }
+		exit(97); }
 	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
 	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]),
-		exit(98); 
-        }
+		exit(98); }
 	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99); 
-        }
+		exit(99); }
 	while (l_read == 1024)
 	{
 		l_read = read(file_from, content, 1024);
