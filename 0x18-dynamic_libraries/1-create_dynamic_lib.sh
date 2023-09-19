acc30a915031431410ebda6f0002fd28c17e4d3f
khalilharir <khalil.harir2@gmail.com>
2023-09-19 13:05:40 -0700

    dynam

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -fPIC -c *.c
-gcc -shared *.o -o liball.s
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
+gcc -shared *.o -o liball.so *.o
 export LD_LIBRARY_PATH=. :$LD_LIBRARY_PATH
